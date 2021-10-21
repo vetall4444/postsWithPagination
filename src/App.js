9c0996d93ca9597f2b707fc5d2f618b67ed76609
Vitalii <vetall4444@mail.ru>
2021-10-21 23:29:23 +0500

    add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import PhotoServices from "./services/service-json";
 import React from "react";
-const service = new PhotoServices();
 function App() {
-  service.getPosts().then((items) => console.log(items));
-  service.getComments(5).then((items) => console.log(items));
-
   return <div className="App"></div>;
 }
 
